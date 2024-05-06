c1d5df9b02d98b371afaaa5a21f8d2d674f88368
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2024-05-06 14:43:53 +0900

    ConversationBufferMemory

--- src/main.py
+++ src/main.py
@@ -1,42 +1,11 @@
-from langchain import LLMChain
+from langchain.chains import ConversationChain
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts import PromptTemplate
-from langchain.chains import SimpleSequentialChain
+from langchain.memory import ConversationBufferMemory
 
 chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+conversation = ConversationChain(llm=chat, memory=ConversationBufferMemory())
 
-cot_template = """以下の質問に回答してください。
-
-質問: {question}
-
-ステップバイステップで考えましょう。
-"""
-
-cot_prompt = PromptTemplate(
-  input_variables=["question"],
-  template=cot_template,
-)
-
-cot_chain = LLMChain(llm=chat, prompt=cot_prompt)
-
-summarize_template="""以下の文章を結論だけ一言に要約してください。
-
-{input}
-"""
-
-summarize_prompt = PromptTemplate(
-  input_variables=["input"],
-  template=summarize_template,
-)
-
-summarize_chain = LLMChain(llm=chat, prompt=summarize_prompt)
-
-cot_summarize_chain = SimpleSequentialChain(
-  chains=[cot_chain, summarize_chain]
-)
-
-result = cot_summarize_chain(
-  "私は市場に行って10個のリンゴを買いました。隣人に2つ、修理工に2つ渡しました。それから5つのリンゴを買って1つ食べました。残りのリンゴは何個ですか？"
-)
-
-print(result["output"])
+while True:
+  user_message = input("You: ")
+  ai_message = conversation.run(input=user_message)
+  print(f"AI: {ai_message}")
