99919b8d1d0bdbd452e6be999b744c39d009fd6c
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2024-05-06 14:41:01 +0900

    SimpleSequentialChain

--- src/main.py
+++ src/main.py
@@ -1,33 +1,42 @@
 from langchain import LLMChain
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
-from langchain.output_parsers import PydanticOutputParser
-from pydantic import BaseModel, Field
-
-class Recipe(BaseModel):
-  ingredients: list[str] = Field(description="ingredients of the dish")
-  steps: list[str] = Field(description="steps to make the dish")
-
-parser = PydanticOutputParser(pydantic_object=Recipe)
-
-template = """料理のレシピを考えてください。
-
-{format_instructions}
-
-料理名: {dish}
-"""
-
-prompt = PromptTemplate(
-  template=template,
-  input_variables=["dish"],
-  partial_variables={"format_instructions": parser.get_format_instructions()},
-)
+from langchain.chains import SimpleSequentialChain
 
 chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
 
-chain = LLMChain(prompt=prompt, llm=chat, output_parser=parser)
+cot_template = """以下の質問に回答してください。
 
-recipe = chain.run(dish="カレー")
+質問: {question}
 
-print(type(recipe))
-print(recipe)
+ステップバイステップで考えましょう。
+"""
+
+cot_prompt = PromptTemplate(
+  input_variables=["question"],
+  template=cot_template,
+)
+
+cot_chain = LLMChain(llm=chat, prompt=cot_prompt)
+
+summarize_template="""以下の文章を結論だけ一言に要約してください。
+
+{input}
+"""
+
+summarize_prompt = PromptTemplate(
+  input_variables=["input"],
+  template=summarize_template,
+)
+
+summarize_chain = LLMChain(llm=chat, prompt=summarize_prompt)
+
+cot_summarize_chain = SimpleSequentialChain(
+  chains=[cot_chain, summarize_chain]
+)
+
+result = cot_summarize_chain(
+  "私は市場に行って10個のリンゴを買いました。隣人に2つ、修理工に2つ渡しました。それから5つのリンゴを買って1つ食べました。残りのリンゴは何個ですか？"
+)
+
+print(result["output"])
