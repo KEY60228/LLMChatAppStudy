440ccaf5fe9589e7d2ab514b988dc19ddfb69399
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2024-05-06 14:10:16 +0900

    Output Parsers

--- src/main.py
+++ src/main.py
@@ -1,20 +1,41 @@
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts import (
-  ChatPromptTemplate,
-  PromptTemplate,
-  SystemMessagePromptTemplate,
-  HumanMessagePromptTemplate,
+from langchain.schema import HumanMessage
+from langchain.prompts import PromptTemplate
+from langchain.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+
+class Recipe(BaseModel):
+  ingredients: list[str] = Field(description="ingredients of the dish")
+  steps: list[str] = Field(description="steps to make the dish")
+
+parser = PydanticOutputParser(pydantic_object=Recipe)
+
+format_instructions = parser.get_format_instructions()
+
+template = """料理のレシピを考えてください。
+
+{format_instructions}
+
+料理名: {dish}
+"""
+
+prompt = PromptTemplate(
+  template=template,
+  input_variables=["dish"],
+  partial_variables={"format_instructions": format_instructions},
 )
-from langchain.schema import HumanMessage, SystemMessage
 
-chat_prompt = ChatPromptTemplate.from_messages([
-  SystemMessagePromptTemplate.from_template("あなたは{country}料理のプロフェッショナルです。"),
-  HumanMessagePromptTemplate.from_template("以下の料理のレシピを考えてください\n\n料理名: {dish}")
-])
-
-messages = chat_prompt.format_prompt(country="イギリス", dish="肉じゃが").to_messages()
+formatted_prompt = prompt.format(dish="カレー")
 
 chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
 
+messages = [
+  HumanMessage(content=formatted_prompt)
+]
+
 result = chat(messages)
-print(result.content)
+
+recipe = parser.parse(result.content)
+
+print(type(recipe))
+print(recipe)
