141aeec00a038fe27641e600bbdaefce16f42236
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2024-05-11 23:48:18 +0900

    Text embedding models

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from langchain_community.document_loaders import GitLoader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
 
 def file_filter(file_path):
   return file_path.endswith(".mdx")
@@ -23,3 +24,11 @@ text_splitter = CharacterTextSplitter(
 
 docs = text_splitter.split_documents(raw_docs)
 print(len(docs))
+
+embeddings = OpenAIEmbeddings()
+
+query = "AWSのS3からデータを読み込むためのDocumentLoaderはありますか？"
+
+verctor = embeddings.embed_query(query)
+print(len(vector))
+print(vector)
