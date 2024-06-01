f969c0f272f5557468cb56657f624ecee99d2792
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2024-06-01 22:13:48 +0900

    Agents

--- src/main.py
+++ src/main.py
@@ -1,45 +1,17 @@
-from langchain_community.document_loaders import GitLoader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
-from langchain_community.vectorstores import Chroma
-from langchain.chains import RetrievalQA
-from langchain.chat_models import ChatOpenAI
+from langchain_core.globals import set_verbose
 
-def file_filter(file_path):
-  return file_path.endswith(".mdx")
+set_verbose(True)
 
-loader = GitLoader(
-  clone_url="https://github.com/langchain-ai/langchain",
-  repo_path="./langchain",
-  branch="master",
-  file_filter=file_filter,
-)
-
-print("Loading documents...")
-
-raw_docs = loader.load()
-print(len(raw_docs))
-
-print("=========================================")
-
-text_splitter = CharacterTextSplitter(
-  chunk_size=1000,
-  chunk_overlap=0,
-)
-
-docs = text_splitter.split_documents(raw_docs)
-print(len(docs))
-
-print("=========================================")
-
-embeddings = OpenAIEmbeddings()
-db = Chroma.from_documents(docs, embeddings)
-retriever = db.as_retriever()
-
-query = "AWSのS3からデータを読み込むためのDocumentLoaderはありますか？"
+from langchain import hub
+from langchain.agents import AgentExecutor, create_react_agent, load_tools
+from langchain_openai import ChatOpenAI
 
 chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
-qa_chain = RetrievalQA.from_chain_type(llm=chat, chain_type="stuff", retriever=retriever)
+tools = load_tools(["terminal"])
+prompt = hub.pull("hwchase17/react")
+agent = create_react_agent(chat, tools, prompt)
 
-result = qa_chain.run(query)
-print(result)
+agent_chain = AgentExecutor(agent=agent, tools=tools)
+
+result = agent_chain.invoke({"input": "langchainディレクトリにあるファイルの一覧を教えて"})
+print(result["output"])
