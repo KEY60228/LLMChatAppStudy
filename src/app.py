14334145f055319445baef168c9ae4efdcc98535
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2024-06-09 22:13:41 +0900

    Agentを使って外部情報を検索させる

--- src/app.py
+++ src/app.py
@@ -1,7 +1,21 @@
 import os
 import streamlit as st
+from langchain import hub
 from langchain_openai import ChatOpenAI
 from langchain.schema import HumanMessage
+from langchain.agents import AgentExecutor, create_openai_tools_agent, load_tools
+from langchain_community.callbacks import StreamlitCallbackHandler
+
+def create_agent_chain():
+    chat = ChatOpenAI(
+        model_name="gpt-3.5-turbo",
+        temperature=0.5,
+        streaming=True,
+    )
+    tools = load_tools(["ddg-search", "wikipedia"])
+    prompt = hub.pull("hwchase17/openai-tools-agent")
+    agent = create_openai_tools_agent(chat, tools, prompt)
+    return AgentExecutor(agent=agent, tools=tools)
 
 st.title("langchain-streamlit-app")
 
@@ -20,12 +34,12 @@ if prompt:
     with st.chat_message("user"):
         st.markdown(prompt)
     with st.chat_message("assistant"):
-        chat = ChatOpenAI(
-            model_name="gpt-3.5-turbo",
-            temperature=0.5,
+        callback = StreamlitCallbackHandler(st.container())
+        agent_chain = create_agent_chain()
+        response = agent_chain.invoke(
+            {"input": prompt},
+            {"callbacks": [callback]},
         )
-        messages = [HumanMessage(content=prompt)]
-        response = chat(messages)
-        st.markdown(response.content)
+        st.markdown(response["output"])
 
     st.session_state.messages.append({"role": "assistant", "content": response})
