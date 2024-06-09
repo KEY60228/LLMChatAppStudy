8350eb89b38961aedde1ea1841fb305098c46320
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2024-06-09 21:29:34 +0900

    入力内容と応答を画面に表示する

--- src/app.py
+++ src/app.py
@@ -3,4 +3,10 @@ import streamlit as st
 st.title("langchain-streamlit-app")
 
 prompt = st.chat_input("What is up?")
-print(prompt)
+
+if prompt:
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    with st.chat_message("assistant"):
+        response = "Hey"
+        st.markdown(response)
