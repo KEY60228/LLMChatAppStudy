a64b1855b1a65fac9a84fbef5971b009a8e004a5
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2024-06-09 22:23:32 +0900

    チャットの会話履歴をふまえて応答させる

--- src/app.py
+++ src/app.py
@@ -4,9 +4,11 @@ from langchain import hub
 from langchain_openai import ChatOpenAI
 from langchain.schema import HumanMessage
 from langchain.agents import AgentExecutor, create_openai_tools_agent, load_tools
+from langchain.memory import ConversationBufferMemory
 from langchain_community.callbacks import StreamlitCallbackHandler
+from langchain_community.chat_message_histories import StreamlitChatMessageHistory
 
-def create_agent_chain():
+def create_agent_chain(history):
     chat = ChatOpenAI(
         model_name="gpt-3.5-turbo",
         temperature=0.5,
@@ -14,32 +16,31 @@ def create_agent_chain():
     )
     tools = load_tools(["ddg-search", "wikipedia"])
     prompt = hub.pull("hwchase17/openai-tools-agent")
+    memory = ConversationBufferMemory(
+        chat_memory=history,
+        memory_key="chat_history",
+        return_messages=True,
+    )
     agent = create_openai_tools_agent(chat, tools, prompt)
-    return AgentExecutor(agent=agent, tools=tools)
+    return AgentExecutor(agent=agent, tools=tools, memory=memory)
 
 st.title("langchain-streamlit-app")
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+history = StreamlitChatMessageHistory()
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+for message in history.messages:
+    st.chat_message(message.type).write(message.content)
 
 prompt = st.chat_input("What is up?")
 
 if prompt:
-    st.session_state.messages.append({"role": "user", "content": prompt})
-
     with st.chat_message("user"):
         st.markdown(prompt)
     with st.chat_message("assistant"):
         callback = StreamlitCallbackHandler(st.container())
-        agent_chain = create_agent_chain()
+        agent_chain = create_agent_chain(history)
         response = agent_chain.invoke(
             {"input": prompt},
             {"callbacks": [callback]},
         )
         st.markdown(response["output"])
-
-    st.session_state.messages.append({"role": "assistant", "content": response})
